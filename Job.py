54c854e2f844ddbf2670055564e773913d0d77ab
ruelle <ruelle@ruelle-System-Product-Name.(none)>
2016-01-25 23:23:49 +0100

    added Job (incomplete)

---
+++ Job.py
@@ -0,0 +1,42 @@
+import java.util.Date as Date
+import java.util.TimeZone as TimeZone
+import org.json.JSONObject as JSONObject
+from Sched import Sched
+
+class Job:
+	def __init__( self, desc, tz = TimeZone.getDefault() ):
+		self.id = desc.getString( "id" )
+		self.tz = tz
+		self.last = None
+		self.next = None
+		self.scheds = []
+		a = desc.getJSONArray( "scheds" )
+		for i in range( a.length() ):
+			self.scheds.append( Sched( a.getJSONObject( i ) ) )
+		self.cmd = desc.getString( "cmd" )
+
+	def dump( self, prefix = "", headprefix = "" ):
+		print headprefix + "id:   \t" + self.id
+		print headprefix + "tz:   \t" + self.tz.getID()
+		if self.last != None:
+			print headprefix + "last: \t" + self.last.toString()
+		if self.next != None:
+			print headprefix + "next: \t" + self.next.toString()
+		for i in self.scheds:
+			print headprefix + "sched:\t+"
+			i.dump( prefix + "      \t|- " )
+		print headprefix + "cmd:  \t" + self.cmd
+
+	def setState( self, data ):
+		if data.has( "last" ):
+			self.last = Date( data.getInt( "last" ) * 1000 )
+		if data.has( "next" ):
+			self.next = Date( data.getInt( "next" ) * 1000 )
+
+	def getState( self ):
+		res = JSONObject()
+		if self.last != None:
+			res.put( "last", self.last.getTime() / 1000 )
+		if self.next != None:
+			res.put( "next", self.next.getTime() / 1000 )
+		return res
